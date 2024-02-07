6ae377efeacf1e30b9a1ac97f15bac0370acc4b5
aldi nugroho <nugroho.aaldi@gmail.com>
2024-02-07 11:34:31 +0700

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      themeMode: ThemeMode.light,
       routerConfig: mainRouter,
     );
   }
